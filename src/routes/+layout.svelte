9b867b71671731c38bfee098c4aeacdf8e3d37d3
ferboinkles <ferboinkles@gmail.com>
2025-12-12 17:31:49 +0000

    BIG COMMIT Added national park font (took way too long) messed around with the background for ages because i tried to follow CSS documentation T-T and uhh yeah I did NOT commit fraud whoever is reviewing me

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,19 @@
 <script>
-	import "./layout.css";
-	import favicon from "$lib/assets/favicon.svg";
-
 	let { children } = $props();
+	import "./layout.css";
 </script>
 
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
+<div class="national-park">
+	{@render children?.()}
+</div>
 
-{@render children()}
+<style>
+	@import url("https://fonts.googleapis.com/css2?family=National+Park:wght@200..800&display=swap");
+
+	.national-park {
+		font-family: "National Park", sans-serif;
+		font-optical-sizing: auto;
+		font-weight: 600;
+		font-style: normal;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,7 @@
-<button class="bg-red-600">Initial commit</button>
+<script>
+	import { goto } from "$app/navigation";
+</script>
+
+<div>
+	<h4 class="text-center text-4xl text-white">Welcome to the Cat-alogue!</h4>
+</div>
