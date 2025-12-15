efaf589c73e830ecb6fa874361a816058be979ac
ferboinkles <ferboinkles@gmail.com>
2025-12-15 17:34:06 +0000

    Added the pusheen picture, a bit of margin and the pip picture but it doesn't do anything lol, oh yeah and i added the title and subtitle since this is basically my first commit :pf

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,5 +3,12 @@
 </script>
 
 <div>
-	<h4 class="text-center text-4xl text-white">Welcome to the Cat-alogue!</h4>
+	<h4 class="text-center text-5xl text-white">Welcome to the Cat-alogue!</h4>
+	<h3 class="text-left text-white text-3xl m-7">The Pusheen Family</h3>
+	<button class="text-left m-4" onclick={goto("/pusheen")}
+		><img alt="pusheen" src="/images/pusheen.webp" />
+	</button>
+	<button class="m-4" onclick={goto("/pusheen")}
+		><img alt="pusheen" src="/images/pip.webp" />
+	</button>
 </div>
