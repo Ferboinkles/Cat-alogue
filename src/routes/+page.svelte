6341b7356cacd800f498e8f01c2510a55c590eae
ferboinkles <ferboinkles@gmail.com>
2025-12-15 17:50:26 +0000

    Changed prettier, and added the stormy buton

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,12 +3,30 @@
 </script>
 
 <div>
-	<h4 class="text-center text-5xl text-white">Welcome to the Cat-alogue!</h4>
-	<h3 class="text-left text-white text-3xl m-7">The Pusheen Family</h3>
-	<button class="text-left m-4" onclick={goto("/pusheen")}
-		><img alt="pusheen" src="/images/pusheen.webp" />
+	<h4 class="text-center text-5xl text-white">
+		Welcome to the Cat-alogue!
+	</h4>
+	<h3 class="text-left text-white text-3xl m-7">
+		The Pusheen Family
+	</h3>
+	<!--Pusheen button-->
+	<button
+		class="text-left m-4"
+		onclick={goto("/pusheen")}
+		><img
+			alt="pusheen"
+			src="/images/pusheen.webp"
+		/>
 	</button>
+	<!--Pip button-->
 	<button class="m-4" onclick={goto("/pusheen")}
 		><img alt="pusheen" src="/images/pip.webp" />
+	</button>
+	<!--Stormy button-->
+	<button class="m-4" onclick={goto("/pusheen")}
+		><img
+			alt="pusheen"
+			src="/images/stormy.webp"
+		/>
 	</button>
 </div>
