c7b25e3562d31fb570a0365a98ff0f54a21b549b
ferboinkles <ferboinkles@gmail.com>
2025-12-29 21:01:52 +0000

    Added all of the cats PROPERLY and added readme and will now add functionality to all cats :)

--- README.md
+++ README.md
@@ -1,38 +1,4 @@
-# sv
-
-Everything you need to build a Svelte project, powered by [`sv`](https://github.com/sveltejs/cli).
-
-## Creating a project
-
-If you're seeing this, you've probably already done this step. Congrats!
-
-```sh
-# create a new project in the current directory
-npx sv create
-
-# create a new project in my-app
-npx sv create my-app
-```
-
-## Developing
-
-Once you've created a project and installed dependencies with `npm install` (or `pnpm install` or `yarn`), start a development server:
-
-```sh
-npm run dev
-
-# or start the server and open the app in a new browser tab
-npm run dev -- --open
-```
-
-## Building
-
-To create a production version of your app:
-
-```sh
-npm run build
-```
-
-You can preview the production build with `npm run preview`.
-
-> To deploy your app, you may need to install an [adapter](https://svelte.dev/docs/kit/adapters) for your target environment.
+Welcome to the Cat-alogue!
+It is a collection of different cats across the internet!
+Pet each cat to find out more about each cat
+This can also be found on cat-alogue.pages.dev
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,28 +6,236 @@
 	<h4 class="text-center text-5xl text-white">
 		Welcome to the Cat-alogue!
 	</h4>
-	<h3 class="text-left text-white text-3xl m-7">
-		The Pusheen Family
+	<h3 class="text-left text-white text-2xl m-7">
+		*Disclaimer - I do not own any of these cat
+		pictures and am distributing them for fun!*
 	</h3>
-	<!--Pusheen button-->
+	<button
+		class="text-left m-4"
+		onclick={goto("/balinese")}
+	>
+		<img
+			alt="balinese"
+			src="/images/balinese.webp"
+		/>
+	</button>
+
+	<button
+		class="text-left m-4"
+		onclick={goto("/biscuit")}
+	>
+		<img
+			alt="biscuit"
+			src="/images/biscuit.webp"
+		/>
+	</button>
+
+	<button
+		class="text-left m-4"
+		onclick={goto("/himalayan")}
+	>
+		<img
+			alt="himalayan"
+			src="/images/himalayan.webp"
+		/>
+	</button>
+
+	<button
+		class="text-left m-4"
+		onclick={goto("/khao_manee")}
+	>
+		<img
+			alt="khao_manee"
+			src="/images/khao_manee.webp"
+		/>
+	</button>
+
+	<button
+		class="text-left m-4"
+		onclick={goto("/korat")}
+	>
+		<img alt="korat" src="/images/korat.webp" />
+	</button>
+
+	<button
+		class="text-left m-4"
+		onclick={goto("/kurilian_bobtail")}
+	>
+		<img
+			alt="kurilian_bobtail"
+			src="/images/kurilian_bobtail.webp"
+		/>
+	</button>
+
+	<button
+		class="text-left m-4"
+		onclick={goto("/laperm_shorthair")}
+	>
+		<img
+			alt="laperm_shorthair"
+			src="/images/laperm_shorthair.webp"
+		/>
+	</button>
+
+	<button
+		class="text-left m-4"
+		onclick={goto("/lynx")}
+	>
+		<img alt="lynx" src="/images/lynx.webp" />
+	</button>
+
+	<button
+		class="text-left m-4"
+		onclick={goto("/maine_coon")}
+	>
+		<img
+			alt="maine_coon"
+			src="/images/maine_coon.webp"
+		/>
+	</button>
+
+	<button
+		class="text-left m-4"
+		onclick={goto("/manx")}
+	>
+		<img alt="manx" src="/images/manx.webp" />
+	</button>
+
+	<button
+		class="text-left m-4"
+		onclick={goto("/munchkin")}
+	>
+		<img
+			alt="munchkin"
+			src="/images/munchkin.webp"
+		/>
+	</button>
+
+	<button
+		class="text-left m-4"
+		onclick={goto("/nebelung")}
+	>
+		<img
+			alt="nebelung"
+			src="/images/nebelung.webp"
+		/>
+	</button>
+
+	<button
+		class="text-left m-4"
+		onclick={goto("/pip")}
+	>
+		<img alt="pip" src="/images/pip.webp" />
+	</button>
+
 	<button
 		class="text-left m-4"
 		onclick={goto("/pusheen")}
-		><img
+	>
+		<img
 			alt="pusheen"
 			src="/images/pusheen.webp"
 		/>
 	</button>
-	<!--Pip button-->
-	<button class="m-4" onclick={goto("/pusheen")}
-		><img alt="pusheen" src="/images/pip.webp" />
+
+	<button
+		class="text-left m-4"
+		onclick={goto("/ragdoll")}
+	>
+		<img
+			alt="ragdoll"
+			src="/images/ragdoll.webp"
+		/>
 	</button>
-	<!--Stormy button-->
-	<button class="m-4" onclick={goto("/pusheen")}
-		><img
-			alt="pusheen"
-			src="/images/stormy.webp"
-			hh
+
+	<button
+		class="text-left m-4"
+		onclick={goto("/scottish_fold")}
+	>
+		<img
+			alt="scottish_fold"
+			src="/images/scottish_fold.webp"
+		/>
+	</button>
+
+	<button
+		class="text-left m-4"
+		onclick={goto("/siamese")}
+	>
+		<img
+			alt="siamese"
+			src="/images/siamese.webp"
+		/>
+	</button>
+
+	<button
+		class="text-left m-4"
+		onclick={goto("/siberian")}
+	>
+		<img
+			alt="siberian"
+			src="/images/siberian.webp"
+		/>
+	</button>
+
+	<button
+		class="text-left m-4"
+		onclick={goto("/singapura")}
+	>
+		<img
+			alt="singapura"
+			src="/images/singapura.webp"
+		/>
+	</button>
+
+	<button
+		class="text-left m-4"
+		onclick={goto("/snowshoe")}
+	>
+		<img
+			alt="snowshoe"
+			src="/images/snowshoe.webp"
+		/>
+	</button>
+
+	<button
+		class="text-left m-4"
+		onclick={goto("/somali")}
+	>
+		<img alt="somali" src="/images/somali.webp" />
+	</button>
+
+	<button
+		class="text-left m-4"
+		onclick={goto("/sphynx")}
+	>
+		<img alt="sphynx" src="/images/sphynx.webp" />
+	</button>
+
+	<button
+		class="text-left m-4"
+		onclick={goto("/stormy")}
+	>
+		<img alt="stormy" src="/images/stormy.webp" />
+	</button>
+
+	<button
+		class="text-left m-4"
+		onclick={goto("/sunflower")}
+	>
+		<img
+			alt="sunflower"
+			src="/images/sunflower.webp"
+		/>
+	</button>
+
+	<button
+		class="text-left m-4"
+		onclick={goto("/turkish_van")}
+	>
+		<img
+			alt="turkish_van"
+			src="/images/turkish_van.webp"
 		/>
 	</button>
 </div>
