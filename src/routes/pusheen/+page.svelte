5dcb45acbcab3a88dd74774893ca18cb0f5911f9
ferboinkles <ferboinkles@gmail.com>
2025-12-29 21:42:58 +0000

    Added all of the folders and files and hackatime should totally record my pain

--- src/routes/pusheen/+page.svelte
+++ src/routes/pusheen/+page.svelte
@@ -1,1 +1,14 @@
-<button>Meow</button>
+<p class="text-3xl text-white">
+	Pusheen is an adorable chubby gray tabby cat who
+	loves to share her playful adventures with the
+	world. With her soft, round shape and cute
+	little paws, she brings joy to anyone who sees
+	her. Pusheen is known for her love of food,
+	often seen enjoying treats like cupcakes and
+	pizza, and she has a sweet, silly personality
+	that makes her even more lovable. Whether she's
+	lounging around or getting into fun activities,
+	Pusheen's charm never fails to make people
+	smile. She’s the kind of cat that would
+	definitely make you say "aww" and smile :3.
+</p>
