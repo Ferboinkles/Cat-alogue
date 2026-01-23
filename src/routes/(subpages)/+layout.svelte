a2a57b9bd331fe117c09928f63ebc4974f183085
ferboinkles <ferboinkles@gmail.com>
2026-01-23 22:00:15 +0000

    Added back button to all pages

---
+++ src/routes/(subpages)/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { goto } from "$app/navigation";
+	let { children } = $props();
+</script>
+
+<button
+	class="max-w-fit rounded-2xl bg-white p-[0.5em] text-left text-xl m-5 text-black"
+	onclick={goto("/")}
+>
+	Back to home
+</button>
+{@render children?.()}
--- src/routes/stormy/+page.svelte
+++
@@ -1,1 +0,0 @@
-<p class="text-3xl text-white">Stormy is Pusheen's sibling
